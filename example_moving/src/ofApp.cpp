84daa0a9d207bee360355830bee4d98255018743
Atsushi Tadokoro <tadokoro@gmail.com>
2014-08-14 14:18:02 +0900

    initial commit

---
+++ example_moving/src/ofApp.cpp
@@ -0,0 +1,95 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+    ofEnableBlendMode(OF_BLENDMODE_ADD);
+    //cam.setFov(80);
+    
+    // ofxVboParticles([max particle number], [particle size]);
+    vboPartciles = new ofxVboParticles(10000, 1000);
+
+    // set friction (0.0 - 1.0);
+    vboPartciles->friction = 0.001;
+    
+    particlePosition = ofVec3f(0, 0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    particlePosition.z  = sin(ofGetElapsedTimef()) * 300;
+    particlePosition.y  = sin(ofGetElapsedTimef() / 2.0) * 100;
+    particlePosition.x  = cos(ofGetElapsedTimef()) * 300;
+
+    for (int i = 0; i < 10; i++) {
+        ofVec3f position = particlePosition;
+        ofVec3f velocity = ofVec3f(ofRandom(-1, 1), ofRandom(-1, 1), ofRandom(-1, 1));
+        ofColor color;
+        color.setHsb(ofRandom(100, 160), 80, 220);
+        
+        // add a particle
+        vboPartciles->addParticle(position, velocity, color);
+    }
+
+    vboPartciles->update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    cam.begin();
+    ofRotate(ofGetElapsedTimef() * 5, 1, 1, 0);
+    vboPartciles->draw();
+    cam.end();
+    
+    ofSetColor(255);
+    ofDrawBitmapString(ofToString(ofGetFrameRate(), 4) + "fps", 10, 20);
+    ofDrawBitmapString("particle num = " + ofToString(vboPartciles->numParticles, 0), 10, 35);
+    ofDrawBitmapString("[f] key : toggle fullscreen", 10, 50);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key == 'f') {
+        ofToggleFullscreen();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
